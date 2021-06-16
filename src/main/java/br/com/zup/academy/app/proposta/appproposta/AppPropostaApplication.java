af4dab2aedc87b98f134d4f29fb53ae1f06bfe7b
[Neto] Antonio Miranda <netomantonio@gmail.com>
2021-06-16 14:09:28 -0300

    Setup do projeto

---
+++ src/main/java/br/com/zup/academy/app/proposta/appproposta/AppPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.app.proposta.appproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPropostaApplication.class, args);
+	}
+
+}
